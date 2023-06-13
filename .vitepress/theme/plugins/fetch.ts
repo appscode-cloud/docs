be617f0136516a7fb7342117040b611cad955f82
M. A. Muhaimin Sakib <sakib@appscode.com>
2023-06-13 15:07:10 +0600

    Add useFetch custom for api calls, also add pinia
    Signed-off-by: M. A. Muhaimin Sakib <sakib@appscode.com>

---
+++ .vitepress/theme/plugins/fetch.ts
@@ -0,0 +1,37 @@
+import { createFetch } from '@vueuse/core'
+import { useCookies } from '@vueuse/integrations/useCookies'
+
+export function getServerDomain(subDomain?: string, inputPort?: string): string {
+  // execute only in client
+  const hostname = window.location.hostname
+  const rootDomain = hostname.replace(
+    /^(console.|kubedb.|marketplace.|deploy.|grafana.|accounts.|modules.|docs.)/,
+    '',
+  )
+  const protocol = window.location.protocol || 'http'
+
+  let host = `${protocol}//${subDomain ? `${subDomain}.` : ''}${rootDomain}`
+  if (hostname.search('bb.test') !== -1) {
+    // dev
+    const port = subDomain ? inputPort || window.location.port : '5998'
+    host += port ? `:${port}` : ''
+  }
+
+  return host
+}
+
+const cookies = useCookies(['_csrf'])
+
+export const useFetch = createFetch({
+  baseUrl: `${getServerDomain('api', '3003')}/api/v1`,
+  options: {
+    timeout: 100000,
+  },
+  fetchOptions: {
+    credentials: 'include',
+    headers: {
+      'x-csrf-token': cookies.get('_csrf'),
+      'X-Requested-With': 'xmlhttprequest',
+    },
+  },
+})
