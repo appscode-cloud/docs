ead646c1cfc57516c67c9aedf1aa6a717272f8dd
mohin7 <mohin@appscode.com>
2023-11-10 11:13:14 +0600

    Add unplugin icons
    Signed-off-by: mohin7 <mohin@appscode.com>

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,6 +1,7 @@
 import process from 'node:process'
 import { defineConfigWithTheme } from 'vitepress'
 import AutoImport from 'unplugin-auto-import/vite'
+import Icons from 'unplugin-icons/vite'
 import HeaderLinks from './header-links'
 import type { ThemeConfig } from './theme/index'
 import SidebarOptions from './sidebar-links.json'
@@ -61,7 +62,7 @@ export default defineConfigWithTheme<ThemeConfig>({
     },
   },
   vite: {
-    plugins: [AutoImport()],
+    plugins: [AutoImport(), Icons({ compiler: 'vue3' })],
     resolve: {
       alias: [
         {
--- .vitepress/theme/components/DocSearch.vue
+++ .vitepress/theme/components/DocSearch.vue
@@ -3,6 +3,8 @@ import { useDebounceFn, useFetch } from '@vueuse/core'
 import { useData } from 'vitepress'
 import { defineAsyncComponent, ref, watch } from 'vue'
 
+const HeroiconsMagnifyingGlass = defineAsyncComponent(() => import('~icons/heroicons/magnifying-glass'))
+
 const DocSearchResults = defineAsyncComponent(() => import('./DocSearchResults.vue'))
 
 const showSearchResults = ref(false)
@@ -63,9 +65,7 @@ watch(searchText, (n) => {
   <div class="ac-navbar-search">
     <div class="search-item">
       <span class="icon">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-        </svg>
+        <heroicons-magnifying-glass />
 
       </span>
       <input
