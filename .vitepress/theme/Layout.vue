8987a798e485373378abf8245327e7b6bc0167d9
mohin7 <mohin@appscode.com>
2023-11-30 18:43:09 +0600

    Update sidebar
    Signed-off-by: mohin7 <mohin@appscode.com>

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -40,67 +40,6 @@ locale.value = lang
   </div>
 </template>
 <style lang="scss">
-.ac-left-sidebar {
-  .menu-list {
-    &.ac-menu-list {
-      padding: 20px;
-      padding-top: 16px !important;
-
-
-      .menu-list {
-        position: relative;
-
-        &::after {
-          position: absolute;
-          content: "";
-          left: 6px;
-          top: 0;
-          width: 1px;
-          height: 100%;
-          background-color: $color-border;
-          z-index: -1;
-        }
-      }
-
-     ul {
-      li {
-        position: relative;
-
-        a {
-          padding: 8px 15px 8px 24px;
-          color: $color-text;
-
-          &::after {
-            width: 12px !important;
-            height: 12px !important;
-            top: 12px !important;
-            left: 0 !important;
-            background-color: $ac-primary !important;
-            border: 1px solid $ac-primary !important;
-            border-radius: 50% !important;
-            opacity: 0;
-          }
-
-          &:hover {
-            color: $ac-primary !important;
-            padding-left: 32px;
-          }
-
-          &.is-active {
-            color: $ac-primary !important;
-
-            &::after {
-              background-color: $ac-primary;
-              border: 1px solid $ac-primary;
-              opacity: 1;
-            }
-          }
-        }
-      }
-     }
-    }
-  }
-}
 
 .ac-system-body {
   grid-template-columns: 350px auto 350px !important;
--- .vitepress/theme/components/SidebarOptions.vue
+++ .vitepress/theme/components/SidebarOptions.vue
@@ -32,99 +32,114 @@ onBeforeUnmount(() => {
     <template v-for="item in options" :key="item.identifier">
       <template v-if="item.children">
         <p class="menu-label ac-menu-label">
-          {{ item.name }}
+          <span> {{ item.name }}</span>
+          <span class="icon">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+              stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+            </svg>
+
+          </span>
         </p>
         <ul class="menu-list">
           <sidebar-options :options="item.children" />
         </ul>
       </template>
       <li v-else>
-        <a :href="wrapLinkWithLang(item.url, true)" :class="{ 'is-active': activeMenuItemIdentifier === item.identifier }">{{ item.name }}</a>
+        <a :href="wrapLinkWithLang(item.url, true)"
+          :class="{ 'is-active': activeMenuItemIdentifier === item.identifier }">{{ item.name }}</a>
       </li>
     </template>
   </aside>
 </template>
 
-<style lang="scss" scoped>
-.ac-menu-label {
-  font-size: 1rem;
-  font-weight: 500;
-  color: $color-heading;
-  letter-spacing: 1px;
-}
-
-.menu-label:not(:last-child) {
-  margin-bottom: 0;
-}
-
-.ac-left-sidebar .menu-list.menu-list li a {
-  font-weight: 400;
-  color: $color-text;
-}
-
-
-// =========
+<style lang="scss">
 .ac-left-sidebar {
+  // .menu-list.ac-menu-list {
+  //   padding: 24px !important;
+  // }
+
+  .ac-menu-label {
+    font-size: 1rem;
+    font-weight: 500;
+    color: $color-heading;
+    letter-spacing: 1px;
+    display: flex;
+    justify-content: space-between;
+
+    .icon {
+     height: 16px;
+     width: 16px;
+     display: flex;
+     align-items: center;
+     color: #555;
+     margin-right: 20px;
+    }
+  }
+
+  .menu-label:not(:last-child) {
+    margin-bottom: 0;
+  }
+
   .menu-list {
-    padding-left: 20px;
+    padding: 20px 0 20px 20px;
+    padding-top: 16px !important;
+    position: relative;
 
-    &.menu-list {
-      padding: 20px;
-      padding-top: 16px !important;
+    p {
+      padding-left: 24px;
+    }
 
-      .menu-list {
-        position: relative;
+    a {
+      padding: 8px 15px 8px 24px;
+      font-weight: 400 !important;
+      color: $color-text;
 
-        &::after {
-          position: absolute;
-          content: "";
-          left: 6px;
-          top: 0;
-          width: 1px;
-          height: 100%;
-          background-color: $color-border;
-          z-index: -1;
-        }
+      &::after {
+        width: 12px !important;
+        height: 12px !important;
+        top: 12px !important;
+        left: 0 !important;
+        background-color: $ac-primary !important;
+        border: 1px solid $ac-primary !important;
+        border-radius: 50% !important;
+        opacity: 0;
       }
 
-     ul {
+      &:hover {
+        color: $ac-primary !important;
+        padding-left: 32px;
+      }
+
+      &.is-active {
+        color: $ac-primary !important;
+
+        &::after {
+          background-color: $ac-primary;
+          border: 1px solid $ac-primary;
+          opacity: 1;
+        }
+      }
+    }
+
+    &::after {
+      position: absolute;
+      content: "";
+      left: 26px;
+      top: 0;
+      width: 1px;
+      height: calc(100% - 16px);
+      background-color: $color-border;
+      z-index: -1;
+    }
+
+    ul {
       li {
         position: relative;
 
-        a {
-          padding: 8px 15px 8px 24px;
-          color: $color-text;
 
-          &::after {
-            width: 12px !important;
-            height: 12px !important;
-            top: 12px !important;
-            left: 0 !important;
-            background-color: $ac-primary !important;
-            border: 1px solid $ac-primary !important;
-            border-radius: 50% !important;
-            opacity: 0;
-          }
-
-          &:hover {
-            color: $ac-primary !important;
-            padding-left: 32px;
-          }
-
-          &.is-active {
-            color: $ac-primary !important;
-
-            &::after {
-              background-color: $ac-primary;
-              border: 1px solid $ac-primary;
-              opacity: 1;
-            }
-          }
-        }
       }
-     }
     }
   }
 }
-
 </style>
